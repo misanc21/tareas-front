a2cff99ff672ee944cffa4383cf631c79b939f95
misanc21 <misanc21@gmail.com>
2020-09-09 16:52:22 -0500

    esqueleto nuevo proyecto form

---
+++ src/components/proyectos/NuevoProyecto.js
@@ -0,0 +1,52 @@
+import React, {Fragment, useState} from 'react';
+
+const NuevoProyecto = () => {
+
+    const [proyecto, setProyecto] = useState({
+        nombre:''
+    })
+    const { nombre } = proyecto
+
+    const handleChangeProyecto = e => {
+        setProyecto({
+            ...proyecto,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const handleSubmitProyecto = e => {
+        e.preventDefault()
+    }
+
+    return (
+        <Fragment>
+            <button
+                type="button"
+                className="btn btn-block btn-primario"
+            >
+                Nuevo proyecto
+            </button>
+            <form
+                className="formulario-nuevo-proyecto"
+                onsubmit={handleSubmitProyecto}
+            >
+                <input 
+                    type="text"
+                    className="input-text"
+                    placeholder="nombre del proyecto"
+                    name="nombre"
+                    onChange={handleChangeProyecto}
+                    value={nombre}
+                />
+                <button
+                    type="submit"
+                    className="btn btn-primario btn-block"
+                >
+                    agregar proyecto
+                </button>
+            </form>
+        </Fragment>
+     );
+}
+ 
+export default NuevoProyecto;
