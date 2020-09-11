febf8b8fa72980b4b04bd2d295e95e0c0aa095a7
misanc21 <misanc21@gmail.com>
2020-09-10 21:59:31 -0500

    agregar context de autenticacion

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import ProyectoState from './context/proyectos/proyectoState'
 import TareasState from './context/tareas/tareasState'
 import AlertaState from './context/alertas/alertaState'
+import AuthState from './context/autenticacion/authState'
 
 import Login from './components/auth/Login'
 import NuevaCuenta from './components/auth/NuevaCuenta'
@@ -15,13 +16,15 @@ function App() {
     <ProyectoState>
       <TareasState>
         <AlertaState>
-          <Router>
-            <Switch>
-              <Route exact path="/" component={Login} />
-              <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
-              <Route exact path="/proyectos" component={Proyectos} />
-            </Switch>
-          </Router>
+          <AuthState>
+            <Router>
+              <Switch>
+                <Route exact path="/" component={Login} />
+                <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
+                <Route exact path="/proyectos" component={Proyectos} />
+              </Switch>
+            </Router>
+          </AuthState>
         </AlertaState>
       </TareasState>
     </ProyectoState>
